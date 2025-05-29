22c9edc880f6020231f94cba37e41d51f7a5ab69
Rehan-H-N <rehanh879@gmail.com>
2025-05-29 20:02:37 +0530

    seed changed

--- seed.js
+++ seed.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Product = require("./models/product");
 
-mongoose.connect("mongodb://localhost:27017/ecommerce", {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
